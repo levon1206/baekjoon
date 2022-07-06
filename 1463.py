6e2da0b398c7ecae6520f4d7eeff854db4f44bdf
levon1206 <dearjin02@naver.com>
2022-07-06 20:03:30 +0900

    2022.07.06백준문제품

---
+++ 1463.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr = [0 for _ in range(n+1)]
+
+for i in range(2, n+1):
+    arr[i] = arr[i-1] + 1
+
+    if i%2 == 0 and arr[i] > arr[i//2] + 1 :
+        arr[i] = arr[i//2]+1
+
+    if i%3 == 0 and arr[i] > arr[i//3] + 1 :
+        arr[i] = arr[i//3] + 1
+
+print(arr[n])
