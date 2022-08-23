04071b69e717281cc0821d2928c735bb5aeefb49
levon1206 <dearjin02@naver.com>
2022-08-23 20:57:38 +0900

    2022.08.23백준문제품

---
+++ 11279.py
@@ -0,0 +1,15 @@
+import heapq as hq
+import sys
+
+n = int(input())
+heap = []
+
+for i in range(n) :
+    x = int(sys.stdin.readline())
+    if x :
+        hq.heappush(heap, (-x , x))
+    else :
+        if len(heap) >= 1 :
+            print(hq.heappop(heap)[1])
+        else :
+            print(0)
